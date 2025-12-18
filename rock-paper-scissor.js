1e9eec65ae561cdbdf1af778cac3f32c3d7c6175
Katsoep <83074378+Katsoep@users.noreply.github.com>
2025-12-18 16:39:45 +0100

    build [core] make function choices for pc and p create function that picks a choice for the computer at random and prompts the human player for a choices and returns it

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,1 +1,40 @@
 console.log("Hello :3")
+// Let  computer choice
+let computerChoice;
+function getComputerChoice() {
+// Create function getComputerChoice
+//    return random value 1 - 3
+//    return equals computer choice
+    let computerChoice;
+    switch(Math.floor(Math.random() * 3) + 1){
+        case 1 :
+            computerChoice = "Rock";
+            break;
+        case 2 :
+            computerChoice = "Paper";
+            break;
+        case 3 :
+            computerChoice = "Scissor";
+            break;
+
+    }
+    console.log(computerChoice);
+    return computerChoice;
+
+}
+// Let human choice 
+let humanChoice;
+function getHumanChoice() {
+    // Create function getHumanChoice
+//    prompt human choice (assuming the prompt is always valid)
+//    return humans choice
+    let humanChoice = prompt("Rock paper or scissor?");
+    console.log(humanChoice);
+    return humanChoice;
+
+}
+
+console.log(getComputerChoice())
+console.log(getHumanChoice())
+
+
