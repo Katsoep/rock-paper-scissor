48d56fae07e2e41ec8def81022a7ba1841c5b6f6
Katsoep <83074378+Katsoep@users.noreply.github.com>
2025-12-22 11:25:29 +0100

    build [core] make function for round and gameplay made a functions that plays one round of rock paper scissor and made another function that plays 5 rounds of a game also keeps a score

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,56 +1,109 @@
 console.log("Hello :3")
-// Let  computer choice
-let computerChoice;
 function getComputerChoice() {
-// Create function getComputerChoice
-//    return random value 1 - 3
-//    return equals computer choice
+
     let computerChoice;
     switch(Math.floor(Math.random() * 3) + 1){
         case 1 :
-            computerChoice = "Rock";
+            computerChoice = "rock";
             break;
         case 2 :
-            computerChoice = "Paper";
+            computerChoice = "paper";
             break;
         case 3 :
-            computerChoice = "Scissor";
+            computerChoice = "scissor";
             break;
-
     }
     console.log(computerChoice);
     return computerChoice;
-
 }
-// Let human choice 
-let humanChoice;
+
 function getHumanChoice() {
-// Create function getHumanChoice
-//    prompt human choice (assuming the prompt is always valid)
-//    return humans choice
 
-
-
-    let humanChoice = prompt("Rock paper or scissor?");
-     console.log(humanChoice);
+    let humanChoice = prompt("Rock paper or scissor?").toLowerCase();
     // check if humanChoice is valid answer before you return it
-    if (humanChoice === "Rock" || humanChoice === "Paper" || humanChoice === "Scissor"){
+
+    if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissor"){
         console.log(humanChoice);
         return humanChoice;
     }
     else {
         alert("Invalid choice, try again")
-            //This causes the humanChoice to be the first one entered, not the correct one
-        // getHumanChoice();
-        // return humanChoice;
     }
-
 }
 
 let humanScore = 0;
 let computerScore = 0;
+let score = "humans: " + humanScore + " vs computer: " + computerScore; 
 
-console.log(getComputerChoice())
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice){
+   
+    if(humanChoice !== computerChoice){
 
+        if (humanChoice === "rock"){
+            if(computerChoice === "scissor"){
+                humanScore++;
+                console.log("Human wins " + humanScore)
+                return;
+            }
+            else if(computerChoice === "paper"){
+                computerScore++;
+                console.log("Computer wins " + computerScore)
+                return;
+            }
+            else{
+                alert("something went wrong");
+                return;
+            }
+        }
+        if (humanChoice === "paper"){
+            if(computerChoice === "rock"){
+                humanScore++;
+                console.log("Human wins " + humanScore)
+                return;
+            }
+            else if(computerChoice === "scissor"){
+                computerScore++;
+                console.log("Computer wins " + computerScore)
+                return;
+            } else {
+                alert("something went wrong");
+                return;
+            }
+        }
+        if (humanChoice === "scissor"){
+            if(computerChoice === "paper"){
+                humanScore++;
+                console.log("Human wins " + humanScore)
+                return;
+            }
+            else if(computerChoice === "rock"){
+                computerScore++;
+                console.log("Computer wins " + computerScore)
+                return;
+            } else {
+                alert("something went wrong");
+                return;
+            }
+        }
+    } else {
+        console.log("It's a tie");
+        return;
+    }
+}
 
+function playGame(){
+
+    let computerSelect = getComputerChoice();
+    let humanSelect = getHumanChoice();
+
+    for (let roundCounter = 1; roundCounter <= 5; roundCounter++){
+        console.log("round " + roundCounter);
+        playRound(humanSelect, computerSelect);
+
+        if(roundCounter === 5){
+        console.log("Final score: Human: " + humanScore + " - Computer: " + computerScore);
+        }
+    }
+}
+
+playGame();
